feb5fe08a92cd533cb30671c572d67636f925ee0
Viktor Kushnir <viktor.kushnir@djangostars.com>
2025-11-23 15:10:36 +0200

    update memorize

--- Tasks/Day2/4-memoize.js
+++ Tasks/Day2/4-memoize.js
@@ -1,13 +1,19 @@
 'use strict';
 
 // implement memoize
-const memoize =
-  (fn, cache = {}) =>
-   (n) =>
-     cache.hasOwnProperty(n)
-      ? cache[n]
-      : cache[n] = fn(n)
+const defaultKeyBuilder = (...args) => args.join(',')
+const keyBuilderTyped = (...args) => args.map(a => `${a.toString()}-${typeof a}`).join(',')
 
-const fib = memoize((n) => (n <= 1 ? n : fib(n - 1) + fib(n - 2)));
+const memoize =
+  (fn, keyBuilder = defaultKeyBuilder, cache = {}) =>
+    (...rest) => {
+      const cacheKey = keyBuilder(...rest)
+      return cache.hasOwnProperty(cacheKey)
+        ? cache[cacheKey]
+        : cache[cacheKey] = fn(...rest)
+    }
+
+
+const fib = memoize((n) => (n <= 1 ? n : fib(n - 1) + fib(n - 2)), keyBuilderTyped);
 
 console.log(fib(10));
